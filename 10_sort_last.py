094cbc3c8d9a18a8e48e5270179b1e121010834c
Rafael Paniago <115939524+rafaelpaniago@users.noreply.github.com>
2024-07-21 21:17:35 -0300

    Update 10_sort_last.py
    Ordenação de tuplas com base no segundo elemento.

--- 10_sort_last.py
+++ 10_sort_last.py
@@ -11,7 +11,8 @@ Dica: Use uma custom key= function para extrair o ultimo elemento de cada tupla.
 """
 def sort_last(tuples):
     # +++ SUA SOLUÇÃO +++
-    return
+    ordenacao_tupla = sorted(tuples, key=lambda x: x[1])
+    return ordenacao_tupla
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
